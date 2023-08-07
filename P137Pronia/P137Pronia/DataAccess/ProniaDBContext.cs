a9a4121332944aecd9c488412f5f71e5539a5ead
Ademx1 <adaman@code.edu.az>
2023-08-08 01:37:58 +0400

    Updated pronia

---
+++ P137Pronia/P137Pronia/DataAccess/ProniaDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using P137Pronia.Models;
+
+namespace P137Pronia.DataAccess
+{
+    public class ProniaDBContext : IdentityDbContext
+    {
+        public ProniaDBContext(DbContextOptions options) : base(options) { }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<AppUser> ApppUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasIndex(p => p.Name).IsUnique();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
